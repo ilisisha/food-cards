e21f5c55d5458e9bc03214c4dfe1b78dea5f320c
ilisisha <iralitsitsina@gmail.com>
2018-11-11 14:40:56 +0300

    Updates

---
+++ precache-manifest.21f881ee5e3525e15a62c3acecf41a03.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "30b5cd0648336b6690f863c474f1f665",
+    "url": "/food-cards/static/media/main-bg.30b5cd06.jpg"
+  },
+  {
+    "revision": "116e37e4a67d9491b4721026e07613ec",
+    "url": "/food-cards/static/media/cat.116e37e4.jpg"
+  },
+  {
+    "revision": "ed5cb43ab50e6658dee2",
+    "url": "/food-cards/static/js/runtime~main.ed5cb43a.js"
+  },
+  {
+    "revision": "945c1e5f2e0d307fd0c7",
+    "url": "/food-cards/static/js/main.945c1e5f.chunk.js"
+  },
+  {
+    "revision": "32dd02942d0bb1c36e00",
+    "url": "/food-cards/static/js/1.32dd0294.chunk.js"
+  },
+  {
+    "revision": "945c1e5f2e0d307fd0c7",
+    "url": "/food-cards/static/css/main.4eab962c.chunk.css"
+  },
+  {
+    "revision": "d25f5d989373a0ae71abbfe27e4ae158",
+    "url": "/food-cards/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/food-cards/precache-manifest.21f881ee5e3525e15a62c3acecf41a03.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/food-cards/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
